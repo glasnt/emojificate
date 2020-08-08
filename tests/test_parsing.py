e1ad8be8c6a7bd7e467ce25e87cdf882b8309f5a
Katie McLaughlin <katie@glasnt.com>
2020-08-08 16:21:15 +1000

    Make tests more complete

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -4,15 +4,22 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from emojificate.filter import emojificate
 
-PYTHON_35 = "🙃"
-PYTHON_36 = "🤣"
-PYTHON_37 = "🥰"
-PYTHON_38 = "🤩"
+# A list of new emoji introduced in the unicodedata set for that version of Python
+# Emoji introduced in later versions won't be available in earlier ones. 
+PYTHON_35 = {"alt": "🙃", "title": "Upside-Down Face"}
+PYTHON_36 = {"alt": "🤣", "title": "Rolling On The Floor Laughing"}
+PYTHON_37 = {"alt": "🥰", "title": "Smiling Face With Smiling Eyes And Three Hearts"}
+PYTHON_38 = {"alt": "🤩", "title": "Grinning Face With Star Eyes"}
 
+def valid(data):
+    alt = data["alt"]
+    title = data["title"]
+    parsed = emojificate(alt)
+    assert alt in parsed
+    assert "alt=\"{}".format(alt) in parsed
 
-def valid(phrase):
-    parsed = emojificate(phrase)
-    assert phrase != parsed
+    assert title in parsed
+    assert "aria-label=\"Emoji: {}".format(title) in parsed
 
 
 @pytest.mark.skipif(sys.version_info.minor < 5, reason="requires Python 3.5 or higher")
@@ -33,12 +40,3 @@ def test_python_37_char():
 @pytest.mark.skipif(sys.version_info.minor < 8, reason="requires Python 3.8 or higher")
 def test_python_38_char():
     valid(PYTHON_38)
-
-
-TEST_NOCHANGE = ["The weather is 35°C"]
-
-
-def test_nochange():
-    for phrase in TEST_NOCHANGE:
-        parsed = emojificate(phrase)
-        assert phrase == parsed
