1107ac5ecd19eb700edf15baeeb5072c092ffcdb
Katie McLaughlin <katie@glasnt.com>
2020-08-08 16:55:06 +1000

    Add a fuzzy check ("Flag" didn't appear in py3.5 title)

--- tests/test_graphemes.py
+++ tests/test_graphemes.py
@@ -2,18 +2,19 @@ import pytest
 from emojificate.filter import emojificate
 
 
-def valid(emoji, title):
+def valid(emoji, title, fuzzy=False):
     parsed = emojificate(emoji)
 
     assert emoji in parsed
     assert 'alt="{}'.format(emoji) in parsed
 
     assert title in parsed
-    assert 'aria-label="Emoji: {}'.format(title) in parsed
+    if not fuzzy:
+        assert 'aria-label="Emoji: {}'.format(title) in parsed
 
 
 def test_flag():
-    valid("🇦🇺", "Flag For Australia")
+    valid("🇦🇺", "Australia", fuzzy=True)
 
 
 def test_pride():
